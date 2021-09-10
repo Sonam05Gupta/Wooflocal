7439e1fba61eadcf297e3e4ec7eb9772e6b5dcb7
Sonam05Gupta <sonam.gupta@dotsquares.com>
2021-09-10 12:46:51 +0530

    added file

---
+++ Src/Screens/AuthScreen/GoogleSigin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function GoogleSigin() {
+    return (
+        <View>
+            <Text>Google Signin</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
